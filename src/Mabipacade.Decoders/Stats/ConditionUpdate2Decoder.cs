37da8af8ab017bfc1e3db8c6b4eda5a318f9cc83
LiYu <mickey9910326@gmail.com>
2026-05-13 06:23:34 +0800

    feat(decoders): add ConditionUpdate2 marker (0xA028)

---
+++ src/Mabipacade.Decoders/Stats/ConditionUpdate2Decoder.cs
@@ -0,0 +1,11 @@
+using Mabipacade.Core.Plugins;
+
+namespace Mabipacade.Decoders.Stats;
+
+public sealed record ConditionUpdate2;
+
+public sealed class ConditionUpdate2Decoder : IPacketDecoder
+{
+    public ushort Op => 0xA028;
+    public object Decode(DecoderInput input) => new ConditionUpdate2();
+}
