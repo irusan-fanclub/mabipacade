0b2d783b2389610648a7f16c10d25525fa51bf15
LiYu <mickey9910326@gmail.com>
2026-05-13 06:22:05 +0800

    feat(decoders): add EntitiesAppear marker (0x5334)

---
+++ src/Mabipacade.Decoders/Entity/EntitiesAppearDecoder.cs
@@ -0,0 +1,11 @@
+using Mabipacade.Core.Plugins;
+
+namespace Mabipacade.Decoders.Entity;
+
+public sealed record EntitiesAppear;
+
+public sealed class EntitiesAppearDecoder : IPacketDecoder
+{
+    public ushort Op => 0x5334;
+    public object Decode(DecoderInput input) => new EntitiesAppear();
+}
---
+++ tests/Mabipacade.Decoders.Tests/Entity/EntitiesAppearDecoderTests.cs
@@ -0,0 +1,22 @@
+using Mabipacade.Core.Model;
+using Mabipacade.Core.Plugins;
+using Mabipacade.Decoders.Entity;
+
+namespace Mabipacade.Decoders.Tests.Entity;
+
+public class EntitiesAppearDecoderTests
+{
+    [Fact]
+    public void Op_Matches()
+    {
+        Assert.Equal((ushort)0x5334, new EntitiesAppearDecoder().Op);
+    }
+
+    [Fact]
+    public void Decodes_ReturnsCorrectType()
+    {
+        var input = new DecoderInput(DateTime.UtcNow, Direction.Inbound, 0x5334, 0UL,
+            Array.Empty<MessageElem>());
+        Assert.IsType<EntitiesAppear>(new EntitiesAppearDecoder().Decode(input));
+    }
+}
