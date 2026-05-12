174666725d9fc1ec6d019e89e1d3f1ee00f5c721
LiYu <mickey9910326@gmail.com>
2026-05-13 06:24:48 +0800

    feat(decoders): add SharpMind marker (0xA41E)

---
+++ src/Mabipacade.Decoders/Misc/SharpMindDecoder.cs
@@ -0,0 +1,11 @@
+using Mabipacade.Core.Plugins;
+
+namespace Mabipacade.Decoders.Misc;
+
+public sealed record SharpMind;
+
+public sealed class SharpMindDecoder : IPacketDecoder
+{
+    public ushort Op => 0xA41E;
+    public object Decode(DecoderInput input) => new SharpMind();
+}
